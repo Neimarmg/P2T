92e91b1a07b14a33b6f6b4c04ee4059cf3f91641
Neimar <neimarmg@hotmail.com>
2017-05-22 21:16:28 -0300

    102

---
+++ sgc/src/app/Componentes-globais/Menus/menu-pai/menu-pai.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-pai',
+  templateUrl: './menu-pai.component.html',
+  styleUrls: ['./menu-pai.component.css']
+})
+export class MenuPaiComponent implements OnInit {
+
+  constructor() { 
+
+    
+  }
+
+  ngOnInit() {
+  }
+
+}
