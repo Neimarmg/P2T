f18adc1666b3b1b37aa120c3404ff1ac1e0f7ea0
Neimar <neimarmg@hotmail.com>
2017-05-31 15:22:35 -0300

    Revert "atualizacao dos links das paginas"
    This reverts commit 55ed942d09f62bd9115c36dfb9bbfdc036578808.

--- sgc/src/app/app.module.ts
+++ sgc/src/app/app.module.ts
@@ -1,31 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { TabelaUsuariosComponent } from './tabela-usuarios/tabela-usuarios.component';
-
 import { UsuarioService } from "app/usuario.service";
 import { FormUsuariosComponent } from './form-usuarios/form-usuarios.component';
 import { Routes, RouterModule } from "@angular/router";
-
 import { HomeComponent } from "app/Componentes-globais/home/home.component";
 import { MenuGlobalComponent } from './Componentes-globais/menu-global/menu-global.component';
-
-import { PessoasComponent} from './cadastros/pessoas/pessoas.component';
 
 
 const rotas:Routes=[
   { path:'lista', component:TabelaUsuariosComponent },
   { path:'novo', component:FormUsuariosComponent },
   { path:'edicao/:ind', component:FormUsuariosComponent },
-
-  { path:'', component:HomeComponent },
-
-  { path:'pessoa', component:PessoasComponent}
-  
+  { path:'',redirectTo:'/lista',pathMatch:'full'},
+  { path:'',redirectTo:'/home',pathMatch:'full' },
+  { path:'',redirectTo:'/menu-global',pathMatch:'full' }
 ];
 
 @NgModule({
@@ -34,8 +27,7 @@ const rotas:Routes=[
     TabelaUsuariosComponent,
     FormUsuariosComponent,
     HomeComponent,
-    MenuGlobalComponent,
-    PessoasComponent
+    MenuGlobalComponent
 
   ],
   imports: [
