388a50afdb82b7887d76f973eb3352559cd2dd09
Neimar <neimarmg@hotmail.com>
2017-06-17 18:02:20 -0300

    17062017 1800
    Criação do ws antes dos testes

--- SgcWs/src/java/Service/BancoService.java
+++ SgcWs/src/java/Service/BancoService.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Service;
 
 import Model.Bancos;
@@ -11,16 +6,54 @@ import java.util.List;
 import javax.ejb.Stateless;
 
 /**
- *
  * @author neimarmoises
  */
 @Stateless
 public class BancoService {
     private List<Bancos> listaDeBancos; 
 
-}
-    public BancoServices() {
-         Li
+    
+    public BancoService() {
+        listaDeBancos = new ArrayList<>();
     }
     
+    
+    public List<Bancos> listar(){
+        return listaDeBancos;
+    }
+    
+    
+    public Bancos buscarPorCodigo(int cod){
+        for (Bancos b : listaDeBancos) {
+            if (b.getCodBanco() == cod){
+                return new Bancos(b.getCodBanco(), b.getNroBanco(), b.getDescricao(), b.getWebPage());
+            }
+        }
+         return null;
+    }
+    
+    
+    private int getIndice(int codigo){
+        for (int i = 0; i < listaDeBancos.size(); i++) {
+            if (listaDeBancos.get(i).getCodBanco() == codigo){
+                return i;
+            }
+        }
+        return -1;     
+    }
+   
+    
+    public void insere(Bancos banco){
+        listaDeBancos.add(banco);
+    }
+    
+    
+    public void atualiza(Bancos banco){
+        listaDeBancos.set(this.getIndice(banco.getCodBanco()), banco);
+    }
+    
+    
+     public void exclui(Bancos banco){
+        listaDeBancos.remove(this.getIndice(banco.getCodBanco()));
+    }    
 }
