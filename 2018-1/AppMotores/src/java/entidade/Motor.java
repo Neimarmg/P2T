7ae823e5492b8994c0b702cee9e1c3c6483e13e3
Neimarmg <neimarmg@hotmail.com>
2018-03-08 10:58:10 -0300

    Comit 1 do semestre

---
+++ 2018-1/AppMotores/src/java/entidade/Motor.java
@@ -0,0 +1,58 @@
+package entidade;
+
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author lhries
+ */
+@XmlRootElement
+public class Motor implements Serializable{
+    private int id;
+    private String nome, descricao, uso;
+
+    public Motor() {
+    }
+
+    public Motor(int id, String nome, String descricao, String uso) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.uso = uso;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public String getUso() {
+        return uso;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setUso(String uso) {
+        this.uso = uso;
+    }
+    
+    
+}
